cbeeff3ad035462df5cbe7411efc64a71b7ffa9e
Rajkrithika <mail2krithika1@gmail.com>
2022-11-08 20:25:50 -0500

    Validation

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,14 @@
+function login() {
+
+
+    (validation.revalidate().then(isValid => {
+    if (isValid) {
+        window.location.href = "./teacher.html";
+    }    })
+    )
+
+}
+
 const loginText = document.querySelector(".title-text .login");
 const loginForm = document.querySelector("form.login");
 const loginBtn = document.querySelector("label.login");
@@ -14,50 +25,4 @@ loginBtn.onclick = (()=>{
 signupLink.onclick = (()=>{
   signupBtn.click();
   return false;
-});
-
-function login() {
-    var loginEmailId = document.getElementById("loginEmailId").value;
-    var loginPassword = document.getElementById("loginPassword").value;
-    console.log("Login Email :: "+loginEmailId);
-    console.log("Login Password :: "+loginPassword);
-    
-    if (loginEmailId == "" || loginPassword == "" ) {
-        alert("Can't Login With Empty Email/Password")
-    } else {
-        fetch('http://localhost:5000/login')
-            .then(response => response.json())
-            .then(data => console.log(data));
-        alert("UNDER DEVELOPMENET !!!");
-    }
-}
-
-function register() {
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
-    var emailId = document.getElementById("emailId").value;
-    var phoneNumber = document.getElementById("phoneNumber").value;
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmPassword").value;
-    var userType = document.getElementById("userType").value;
-
-    if (password != confirmPassword) {
-        alert("Password Does Not Match!!!!");
-        return;
-    } else if (userType == "" || userType == undefined){
-        alert("User Category Can not be Blank")
-    }
-    else {
-        fetch('http://localhost:5000/register', {
-            headers: {
-                'Content-type': 'application/json'
-            }, 
-            method: "POST",
-            body: JSON.stringify({"firstName": firstName, "lastName":lastName, "emailId":emailId, "phoneNumber":phoneNumber, 
-            "password":password, "userType": userType})
-        })
-            .then(response => response.json())
-            .then(data => alert("Registration SuccessFul !!!! Future pages under developmenyt."));
-    
-    }   
-}
+});   
